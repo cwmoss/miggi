085d2f2848da54a6aa8dffad8a6691c452dd5b0c
Robert Wagner <rw@20sec.net>
2025-08-25 18:42:14 +0200

    mysql

--- src/ddl/mysql.php
+++ src/ddl/mysql.php
@@ -8,3 +8,51 @@ CREATE TABLE products (
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 */
+
+namespace miggi\ddl;
+
+use LogicException;
+
+class mysql extends driver {
+
+    public function column_definition(column $col, array $keys): string {
+        $ddl = [$col->name];
+        $type = $this->type($col);
+        $ddl[] = $type;
+        if ($col->auto) {
+            $ddl[] = "PRIMARY KEY AUTOINCREMENT NOT NULL"; # todo nn
+            if (count($keys) > 1) {
+                // print_r($keys);
+                throw new LogicException("can't have multiple keys with autoincrement feature");
+            }
+        }
+        if ($col->pk) {
+            if (count($keys) == 1 && !$col->auto) {
+                $ddl[] = "PRIMARY KEY";
+            }
+        }
+        if ($col->unique) {
+            $ddl[] = "UNIQUE";
+        }
+        if ($col->notnull && !$col->pk && !$col->auto) {
+            $ddl[] = "NOTNULL";
+        }
+        if ($col->default !== null) {
+            $ddl[] = "DEFAULT \"$col->default\"";
+        }
+        if ($col->max) {
+            // $ddl[] = "CHECK(LENGTH($col->name)<=$col->max)";
+        }
+        return join(" ", $ddl);
+    }
+
+    public function type(column $col): string {
+        return match ([$col->type, $col->size]) {
+            [type::string, $col->size] => 'VARCHAR($col->size)',
+            [type::number, $col->size] => 'INTEGER',
+            [type::datetime, $col->size] => 'DATETIME',
+            [type::timestamp, $col->size] => 'DATETIME',
+            default => 'TEXT'
+        };
+    }
+}
